ccc3e862441ff17fe7863182c91ecec25f833060
Luke Mclaren <luke@skiomusic.com>
2015-09-21 14:12:53 -0700

    Parents suck

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import rsa
+import time
 
 def validate():
+
 	public_key = '-----BEGIN RSA PUBLIC KEY-----\nMEgCQQDoTcl289XVSXh1PEgMl4rlXN73ycB3GvEyqb9VXMocsFJ2aBvH9gGTJNxy\nx0WH7hAXhuAfRiS5WQpZ+sUKlXFzAgMBAAE=\n-----END RSA PUBLIC KEY-----\n'
 	signature = ''
 
@@ -12,14 +14,16 @@ def validate():
 
 	public_key = rsa.PublicKey.load_pkcs1(public_key, format='PEM')
 
-	date = f.readline().rstrip()
-	
+	date = f.readline().rstrip()	
+
 	for line in f:
 		signature = signature+line
 
 	try:
 		rsa.verify(date, signature, public_key)
-		execute()
+		date = time.strptime(date, "%d/%m/%Y")
+		if date > time.localtime():
+			execute()
 	except:
 		print "License file not valid. Cyber police notified."
 
